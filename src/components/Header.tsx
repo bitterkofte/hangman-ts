b201b69f855b89b5f9ca469e6476dda20158ab16
Hasan Talha Çelik <35665454+bitterkofte@users.noreply.github.com>
2024-02-20 17:20:16 +0300

    img error fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
+import head from '../assets/head.svg'
+
 export const Header = () => {
   return (
     <nav className="header">
       <h2 className="title">Hangman.ts</h2>
-      <img src="/src/assets/head.svg" className="logo" alt="head" />
+      <img src={head} className="logo" alt="head" />
     </nav>
   )
 }
